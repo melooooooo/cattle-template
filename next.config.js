bf6785711403f874c1c14157d3ff354d16d973fe
Xym470 <analiserpmdgh@gmail.com>
2025-05-23 13:51:37 +0800

    更新项目文件

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['crazy-cattle3d.org'],
+  },
+  // 定义环境变量，确保在浏览器端可用
+  env: {
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL || 'https://crazy-cattle3d.org',
+  },
+}
+
+module.exports = nextConfig 
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -10,11 +8,6 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
-    domains: ['crazy-cattle3d.org'],
-  },
-  // 定义环境变量，确保在浏览器端可用
-  env: {
-    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL || 'https://crazy-cattle3d.org',
   },
 }
 
